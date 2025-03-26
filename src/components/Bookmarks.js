999caf3b7de708b9bbbfbff34f5e7f5fd1d5dba6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-26 12:15:52 -0400

    Update

--- src/components/Bookmarks.js
+++ src/components/Bookmarks.js
@@ -1,6 +1,22 @@
-function Bookmarks(){
+function Bookmarks({bookmarks}){
     return(
         <>
+        <div>
+            <h2>Bookmarked Articles</h2>
+
+            {bookmarks.length===0 ? <p>No bookmarks yet.</p>:(
+                bookmarks.map((article,index)=>(
+                    <div>
+
+
+
+
+
+                        
+                    </div>
+                ))
+            )}
+        </div>
         
         </>
     );
