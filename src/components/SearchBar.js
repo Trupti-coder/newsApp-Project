7367f0321c072ec1dd9a64c936ac1286923309a2
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-26 10:55:57 -0400

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,6 +12,17 @@ function SearchBar({onSearch}){
     
     return(
         <>
+        <div >
+
+            <input
+            type="text"
+            placeholder="Search for news..."
+            value={query}
+            onChange={(event)=>setQuery(event.target.value)}
+
+            />
+
+        </div>
         </>
     );
 }
