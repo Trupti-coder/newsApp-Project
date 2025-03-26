e298343b07671a37309db29cf4de27ea502dc5f6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-26 12:19:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[articles,setArticles]=useState([]);
+  const[bookmarks,setBookmarks]=useState([]);
   return (
     <>
     </>
