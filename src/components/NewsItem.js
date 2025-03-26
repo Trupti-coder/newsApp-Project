cdf4ade22e4fe6035c3e19f96b7e4027a63e5dfc
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-26 12:07:56 -0400

    Update

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,6 +1,12 @@
-function NewsItem(){
+function NewsItem({article,onBookmark}){
+
     return(
         <>
+
+        <div>
+            <h3>{article.title}</h3>
+            <p>{article.description || 'no description available'}</p>
+        </div>
         
         
         </>
