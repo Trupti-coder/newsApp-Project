173e158ed3e0f97a85874abee714e3bf0026099e
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-26 12:13:16 -0400

    Update NewsList.js

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,9 +1,13 @@
 import NewsItem from "./NewsItem";
-function NewsList({article,onBookmark}){
+function NewsList({articles,onBookmark}){
     return(
         <>
 
         <div>
+
+        {articles.map((article, index) => (
+        <NewsItem key={index} article={article} onBookmark={onBookmark} />
+      ))}
 
            
         </div>
