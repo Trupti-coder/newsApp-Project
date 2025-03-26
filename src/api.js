47f71a55ec9fcc07aaa1cf16ada37cfebcca5155
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-25 23:18:23 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,14 @@
 const API_KEY="cf7167dac7d54e7eb9713c2d4a28da18";
-const BASE_URL=""
+const BASE_URL="https://newsapi.org/v2/everything?q=tesla&from=2025-02-25&sortBy=publishedAt&apiKey=cf7167dac7d54e7eb9713c2d4a28da18";
+
+export const fetchNews = async (category = '', query = '') =>{
+
+
+
+    try {
+        const url = `${BASE_URL}/top-headlines?country=us&apiKey=${API_KEY}${
+          category ? `&category=${category}` : ''
+        }${query ? `&q=${query}` : ''}`;
+
+
+}
