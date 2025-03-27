2a586372ed1d27f358335a577ff0dd97d92ab7d7
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-03-27 13:13:07 -0400

    Update

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,11 +5,11 @@ function NewsItem({article,onBookmark}){
     return(
         <>
 
-        <div>
+        <div className={styles.container}>
             <h3>{article.title}</h3>
             <p>{article.description || 'no description available'}</p>
             <a href={article.url} target="_blank" rel="noreferrer" className={styles.content}>Read More</a><br></br><br></br>
-            <button onClick={()=>onBookmark(article)}>onBookmark</button>
+            <button onClick={()=>onBookmark(article)} className={styles.btn}>onBookmark</button>
         </div>
         
         
